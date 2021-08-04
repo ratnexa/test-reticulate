758e1189634d60b7ba46e9f8ad3a9ff621b11005
Merqueo <samezquita@merqueo.com>
2021-08-03 21:22:48 -0500

    add scripts test

---
+++ scriptR1.R
@@ -0,0 +1,7 @@
+library(dplyr)
+testdf <- data.frame(
+  a = c(1,2,3,5,6),
+  b = c(1,2,3,4,5)
+)
+
+print(testdf)
---
+++ test1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+testdf = pd.DataFrame({
+    'a': [1,2,3,5,6],
+    'b': [1,2,3,5,6]
+})
+
+print(testdf)
